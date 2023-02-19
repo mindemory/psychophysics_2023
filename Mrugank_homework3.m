e39a63461bad2103cade51153c9c8ad1aeb4e75a
Mrugank Dake <mrugankdake@gmail.com>
2023-02-19 19:00:00 +0100

    Minor adds to HW3

--- Mrugank_homework3.m
+++ Mrugank_homework3.m
@@ -6,20 +6,20 @@ clear; close all; clc;
 cohLevs = linspace(-1, 1, 20);
 Ncoh = length(cohLevs);
 ntrials = 1e2;
-nsubs = 7;
+nsubs = 4;
 eps = 1e-6; % A small value to avoid float-point errors
 
 psyfuncGenerator = @(x, p) (0.5*0)+(1-0).*normcdf(x, p(1), p(2));
 PSEs = [0 0 0 0;
     -0.3 -0.3 0.3 0.3;
-    -0.8 -0.3 0.3 0.8;
-    -0.8 -0.3 0.3 0.8];
+    -0.6 -0.3 0.3 0.6;
+    -0.6 -0.3 0.3 0.6];
 
 slp = 4;
 slopes = [slp slp slp slp;
           slp slp slp slp;
           slp slp slp slp;
-          slp*10 slp slp slp*10];
+          slp*2.5 slp slp slp*2.5];
 
 data = zeros(size(PSEs, 1), size(PSEs, 2), Ncoh, nsubs, ntrials);
 color_vect = ["r*", "b*", "k*", "m*"];
@@ -29,31 +29,32 @@ legnames = {'Adapt L: Attend yes', ...
             'Adapt R: Attend no'};
 
 figure();
-for modelN =  1:size(PSEs, 1)
-    subplot(2, 2, modelN)
-    hold on;
-    for ttype = 1:size(PSEs, 2)
-        for sub = 1:nsubs
+for sub = 1:nsubs
+    for modelN =  1:size(PSEs, 1)
+        subplot(2, 2, modelN)
+        hold on;
+        for ttype = 1:size(PSEs, 2)
             this_PSE = PSEs(modelN, ttype) + randn(1, 1)*PSEs(modelN, ttype)/10;
             this_slope = slopes(modelN, ttype) + randn(1, 1)*slopes(modelN, ttype)/10;
             this_psyfunc = psyfuncGenerator(cohLevs, [this_PSE, 1/this_slope]);
             this_data = rand(Ncoh, ntrials) < repmat(this_psyfunc', 1, ntrials);
             data(modelN, ttype, :, sub, :) = this_data;
-            plot(cohLevs, squeeze(mean(this_data, 2)), color_vect(ttype));%, 'DisplayName', legnames{ttype})
+            plot(cohLevs, squeeze(mean(this_data, 2)), color_vect(ttype));
         end
+        xlabel('Coherence Level')
+        ylabel('Prob correct for Right')
+        title(['Model: ' num2str(modelN)])
     end
-    xlabel('Coherence Level')
-    ylabel('Prob correct for Right')
-    title(['Model: ' num2str(modelN)])
 end
 
+%%
 % Initializes variables for sign, attention, and slope change, needed for different model conditions.
 sign_mu = [-1 -1 1 1];
 attn_mu = [-1 0 0 1];
 sig_change = [1 0 0 1];
 % Defines the negative log-likelihood function that will be minimized.
 nLogL = @(NR, NT, p) -sum(NR.*log(psyfuncGenerator(cohLevs, [p(1) p(2)])+eps) + ...
-             (NT-NR).*log(1-psyfuncGenerator(cohLevs, [p(1) p(2)])+eps));
+                         (NT-NR).*log(1-psyfuncGenerator(cohLevs, [p(1) p(2)])+eps));
         
 NLLs = zeros(nsubs, size(PSEs, 2), size(PSEs, 2)); % Shape is (subjects * model_groudtruth * model_estim)
 
@@ -75,15 +76,15 @@ flat_prior = 1/length(mu2_range)*ones(1,length(mu2_range));
 M2_jointprior = (1./sig2_range) .* flat_prior;
 M2_jointprior = M2_jointprior';
 M2_jointpriornorm = M2_jointprior/sum(M2_jointprior, 'all', 'omitnan');
-figure
-set(gcf,'position',[10,10,1000,400])
-%subplot(1,2,1)
-imagesc(sig2_range, flat_prior, M2_jointpriornorm)
-box off
-title('Model Two Prior')
-xlabel('\sigma')
-ylabel('\mu')
-colorbar
+% figure
+% set(gcf,'position',[10,10,1000,400])
+% %subplot(1,2,1)
+% imagesc(sig2_range, flat_prior, M2_jointpriornorm)
+% box off
+% title('Model Two Prior')
+% xlabel('\sigma')
+% ylabel('\mu')
+% colorbar
 [MU2,SIGMA2]= meshgrid(mu2_range,sig2_range);
 D2 = NaN(nsubs, size(PSEs, 2), res);
 
@@ -103,14 +104,16 @@ sig4_2_range = linspace(lb_M4(4), ub_M4(4), res);
 D4 = NaN(nsubs, size(PSEs, 2), res, res, res, res);
 
 options = optimoptions(@fmincon, 'MaxIterations', 1e5, 'Display', 'off');
+mfit_curves = NaN(nsubs, size(PSEs, 2), size(PSEs, 2), len(cohLevs));
+
 for sub = 1:nsubs
     for modelN =  1:size(PSEs, 2)
         nR = squeeze(sum(data(modelN, :, :, sub, :), 5));
         
         M1_nlogNL = @(p) sum(arrayfun(@(idx) nLogL(nR(idx, :), ntrials, [0 p]), 1:4));
-        parfor ii = 1:res
-            D1(sub, modelN, ii) = M1_nlogNL(sig1_range(ii));
-        end
+%         parfor ii = 1:res
+%             D1(sub, modelN, ii) = M1_nlogNL(sig1_range(ii));
+%         end
         idx1 = find(D1(sub, modelN, :) == min(D1(sub, modelN, :)));
         bestNLL1 = D1(sub, modelN, idx1);
         interim1 = -D1(sub, modelN, :) + log(prior_M1);
@@ -119,11 +122,14 @@ for sub = 1:nsubs
         c1_norm = max(normalized_interim1(:));
         MEs(sub, modelN, 1) = sum(exp(normalized_interim1(:)))*exp(c1_norm);
         [estP_M1, NLLs(sub, modelN, 1)] = fmincon(M1_nlogNL, init_M1, [], [], [], [], lb_M1, ub_M1, [], options);
+        for ii = 1:4
+            mfit_curves(sub, modelN, ii, :) = psyfuncGenerator(cohLevs, [0 estP_M1]);
+        end
         
         M2_nlogNL = @(p) sum(arrayfun(@(idx) nLogL(nR(idx, :), ntrials, [p(1)*sign_mu(idx) p(2)]), 1:4));
-        parfor ii = 1:res
-            D2(sub, modelN, ii) = M2_nlogNL([mu2_range(ii), sig2_range(ii)]);
-        end
+%         parfor ii = 1:res
+%             D2(sub, modelN, ii) = M2_nlogNL([mu2_range(ii), sig2_range(ii)]);
+%         end
         idx2 = find(D2(sub, modelN, :) == min(D2(sub, modelN, :)));
         bestNLL2 = D2(sub, modelN, idx2);
         interim2 = -D2(sub, modelN, :) + log(M2_jointpriornorm);
@@ -132,6 +138,9 @@ for sub = 1:nsubs
         c2_norm = max(normalized_interim2(:));
         MEs(sub, modelN, 2) = sum(exp(normalized_interim2(:)))*exp(c2_norm);
         [estP_M2, NLLs(sub, modelN, 2)] = fmincon(M2_nlogNL, init_M2, [], [], [], [], lb_M2, ub_M2, [], options);
+        for ii = 1:4
+            mfit_curves(sub, modelN, ii, :) = psyfuncGenerator(cohLevs, [0 estP_M1]);
+        end
         
         M3_nlogNL = @(p) sum(arrayfun(@(idx) nLogL(nR(idx, :), ntrials, [p(1)*sign_mu(idx)+p(2)*attn_mu(idx), p(3)]), 1:4));
 %         parfor ii = 1:res
@@ -145,6 +154,30 @@ for sub = 1:nsubs
         
         M4_nlogNL = @(p) sum(arrayfun(@(idx) nLogL(nR(idx, :), ntrials, [p(1)*sign_mu(idx)+p(2)*attn_mu(idx), p(3)+p(4)*sig_change(idx)]), 1:4));
         [estP_M4, NLLs(sub, modelN, 4)] = fmincon(M4_nlogNL, init_M4, [], [], [], [], lb_M4, ub_M4, [], options);
+    end
+end
+
+figure();
+for ii = 1:4
+    subplot(4, 4, 4*(ii-1)+1); hold on;
+    for ttype = 1:size(PSEs, 2)
+        this_data = data(modelN, ttype, :, sub, :);
+        plot(cohLevs, squeeze(mean(this_data, 2)), color_vect(ttype));%, 'DisplayName', legnames{ttype})
+    end
+    subplot(4, 4, 4*(ii-1)+2); hold on;
+    for ttype = 1:size(PSEs, 2)
+        this_data = data(modelN, ttype, :, sub, :);
+        plot(cohLevs, squeeze(mean(this_data, 2)), color_vect(ttype));%, 'DisplayName', legnames{ttype})
+    end
+    subplot(4, 4, 4*(ii-1)+3); hold on;
+    for ttype = 1:size(PSEs, 2)
+        this_data = data(modelN, ttype, :, sub, :);
+        plot(cohLevs, squeeze(mean(this_data, 2)), color_vect(ttype));%, 'DisplayName', legnames{ttype})
+    end
+    subplot(4, 4, 4*(ii-1)+4); hold on;
+    for ttype = 1:size(PSEs, 2)
+        this_data = data(modelN, ttype, :, sub, :);
+        plot(cohLevs, squeeze(mean(this_data, 2)), color_vect(ttype));%, 'DisplayName', legnames{ttype})
     end
 end
 
